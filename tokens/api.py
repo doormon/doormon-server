6dc1a06560d70bb923951324067d5d79cfad98fd
Will Shackleton <w.shackleton@gmail.com>
2015-01-04 18:03:59 +0000

    Added mobile registration

--- tokens/api.py
+++ tokens/api.py
@@ -1,2 +1,16 @@
 """ api routes
 """
+
+from google.appengine.api import users
+
+from website import app
+from .decorators import login_required
+from .models import User
+
+@app.route('/register/mobile/<registration_id>/')
+@login_required
+def register_mobile(registration_id):
+    user = User.get_by_user(users.get_current_user())
+    user.registration_id = registration_id
+    user.put()
+    return "Registered device against Google account"
--- tokens/models.py
+++ tokens/models.py
@@ -2,5 +2,13 @@ from google.appengine.ext import ndb
 
 class User(ndb.Model):
 	user = ndb.UserProperty(required=True)
-	registration_id = ndb.StringProperty(required=True)
-        api_key = ndb.StringProperty(required=True)
+	registration_id = ndb.StringProperty(required=False)
+        api_key = ndb.StringProperty(required=False)
+
+        @classmethod
+        def get_by_user(cls, user):
+            entity = User.query(User.user == user).get()
+            if entity is None:
+                entity = User(user=user)
+                entity.put()
+            return entity
